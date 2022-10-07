6633d95fdcb3330f18c359a54573f3c5de1f3c83
Vlado <v.vladinov@student.fontys.nl>
2022-10-07 18:11:33 +0200

    Create camera/ground + calculate frustum

--- ai.py
+++ ai.py
@@ -1,4 +1,6 @@
 import bpy
+import math
+import random
 from bpy.props import (StringProperty,
                        BoolProperty,
                        IntProperty,
@@ -29,6 +31,65 @@ class Properties(PropertyGroup):
         max = 10
     )
 
+# Create the scene
+class GenerateScene(bpy.types.Operator):
+    bl_idname = "wm.generate_scene"
+    bl_label = "OPOPOP"
+    
+    def execute(self, context):
+        scene = context.scene
+        properties = scene.custom_properties
+        
+        # clear the scene
+        bpy.ops.object.select_all(action='SELECT')
+        bpy.ops.object.delete(use_global=False)
+        
+        # create camera
+        rnd = random.random()
+        pos_z = rnd * 20.0
+        rot_x = rnd 
+        
+        camera = bpy.ops.object.camera_add(
+            enter_editmode=False,
+            align='WORLD',
+            location=(0, 0, pos_z),
+            rotation=(math.radians(90.0), 0, 0)
+        )
+        
+        camera = context.object
+        camera.scale = (100,100,100)
+        
+        #calculate camera frustum
+        matrix = camera.matrix_basis
+        frame = [matrix @ v for v in camera.data.view_frame(scene=scene)]
+        origin = matrix.to_translation()
+        frame.append(origin)
+        for p in frame:
+            bpy.ops.object.empty_add(location=p)
+        
+        # create ground plane
+        if properties.ground:
+            bpy.ops.mesh.primitive_plane_add(
+                enter_editmode=False,
+                align='WORLD',
+                location=(0, 50, 0),
+                size=200
+            )
+            
+
+        # switch to camera perspective
+        for area in bpy.context.screen.areas:
+            if area.type == 'VIEW_3D':
+                area.spaces[0].region_3d.view_perspective = 'CAMERA'
+                break
+        
+        # deselect all
+        bpy.ops.object.select_all(action='TOGGLE')
+
+
+        return {'FINISHED'}
+
+# Generate section
 class GeneratePanel(bpy.types.Panel):
     bl_label = "Generate"
     bl_idname = "PT_GeneratePanel"
@@ -51,12 +112,11 @@ class GeneratePanel(bpy.types.Panel):
         row.prop(properties, "nrObj")
         row.prop(properties, "ground", icon = "VIEW_PERSPECTIVE")
         
-        row.operator("mesh.primitive_cube_add", text = "Generate", icon = "SCENE_DATA")
+        row.operator(GenerateScene.bl_idname, text = "Generate", icon = "SCENE_DATA")
         row = layout.row()
         row.label(text = (str(properties.nrObj) + " object(s)") + (" with ground" if properties.ground else ""))
         
-        
-
+# Evaluate section
 class EvaluatePanel(bpy.types.Panel):
     bl_label = "Evaluate"
     bl_idname = "PT_EvaluatePanel"
@@ -75,6 +135,7 @@ class EvaluatePanel(bpy.types.Panel):
 # ------------------------------------------------------------------------
 
 classes = (
+    GenerateScene,
     GeneratePanel,
     Properties,
     EvaluatePanel
